64fe09c3117d1ffab18695a8ded6acab8e1fe9e4
Bikram Singh <sbikram@live.com>
2017-12-07 20:38:12 +0530

    updated some code

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 
 	switch r.URL.Path[1:] {
 	case "ping":
-		fmt.Fprint(w, "ponga")
+		fmt.Fprint(w, "pong")
 	case "hi":
 		fmt.Fprint(w, "holla")
 	default:
